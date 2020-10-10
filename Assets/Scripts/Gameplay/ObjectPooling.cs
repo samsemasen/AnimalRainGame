a44602e69c7cd1aebc621452c0e50c253f256c58
samsemasen <sam.semasen@gmail.com>
2020-10-10 23:57:40 +0300

    player controller

---
+++ Assets/Scripts/Gameplay/ObjectPooling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    private static ObjectPooling _instance;
+    public static ObjectPooling SharedInstance { get { return _instance; } }
+
+    public GameObject[] animalsToPool;
+    [HideInInspector]
+    public List<GameObject> pooledAnimals;
+    public int animalCount;
+
+    private void Awake()
+    {
+        // Singleton Pattern
+        if (_instance != null && _instance != this)
+            Destroy(gameObject);
+        else
+            _instance = this;
+
+        pooledAnimals = new List<GameObject>();
+        for (int i = 0; i < animalsToPool.Length; i++) {
+            for (int j = 0; j < animalCount; j++) {
+                GameObject obj = Instantiate(animalsToPool[i]);
+                pooledAnimals.Add(obj);
+                obj.transform.parent = transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+    private float playerSpeed = 6f;
+
+
+    void Update()
+    {
+        float horizantal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizantal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.01f) {
+            controller.Move(direction * playerSpeed * Time.deltaTime);
+        }
+
+    }
+}
